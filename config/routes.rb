2895d9da8538151a6ab3ea514acbef3961ca2788
RayNawara <rjnawara@gmail.com>
2024-08-29 13:30:04 -0400

    Added Docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get "home/index"
+  resources :docs
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
